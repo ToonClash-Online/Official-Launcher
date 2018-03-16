ac1f475ccaef741ab5b839660d5ba3fbc7d52221
Dr. Rain O'Sniffy <31706512+CSMellon@users.noreply.github.com>
2018-03-16 12:45:18 -0400

    Update game_starter.py

--- game_starter.py
+++ game_starter.py
@@ -10,7 +10,6 @@ from launcher_globals import *
 class GameStarter():
 
     def __init__(self):
-        # Start game internal globals
 
         if CURRENT_PLATFORM == 'Linux':
             self.python_path = PYTHON_PATH
@@ -20,16 +19,13 @@ class GameStarter():
 
 
     def launchGame(self):
-        # This still needs work but I guess this is slightly better
         cookie = str((self.uiCallback.uName + self.uiCallback.pWord))
 
-        ### Game starting commands ###
         if CURRENT_PLATFORM == 'Linux':
             cmd_00 = 'export ttiUsername=' + self.uiCallback.uName + ' && export ttiPassword=' + self.uiCallback.pWord + ' && export TTI_GAMESERVER=' + GAME_SERVER + ' && cd src/ && ' + self.python_path + ' -O -m toontown.toonbase.ClientStart ' + cookie
         else:
             cmd_00 = 'set ttiUsername=' + self.uiCallback.uName + ' && set ttiPassword=' + self.uiCallback.pWord + ' && set TTI_GAMESERVER=' + GAME_SERVER + ' && cd "src" && ' + self.python_path + ' -O -m toontown.toonbase.ClientStart ' + cookie
 
-        # Before we run the command lets set the username variables to null
         self.uiCallback.uName = False
         self.uiCallback.pWord = False
         cookie = False
