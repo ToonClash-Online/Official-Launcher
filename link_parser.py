e869ecb81150878a0c1014d8193a7a9e1b49b7f1
Dr. Rain O'Sniffy <31706512+CSMellon@users.noreply.github.com>
2018-03-16 12:46:09 -0400

    Update link_parser.py

--- link_parser.py
+++ link_parser.py
@@ -13,7 +13,6 @@ class LinkParser():
         self.unzip_list = []
 
 
-    # Extract the link data and put the dictonaries into a list form the YAML file
     def extractLinks(self, file_name):
         # Read the YAML file
         with open(file_name, 'r') as stream:
@@ -27,21 +26,15 @@ class LinkParser():
             # Keep the UI active
             self.refreshUI()
 
-
-    # Parses out the data out of dictionaries inside of our list
     def parseLinks(self):
         for link_data in self.link_data:
 
             try:
-            # If the data isn't corrupted, continuing
 
-                # This is a special case, and will dictate if we continue further with the file we're dealing with
                 file_platform = (link_data['platform'])
 
-                # If the file is for our current platform go ahead and process and apend it to the list
                 if file_platform == CURRENT_PLATFORM or file_platform == PLATFORM_ALL:
 
-                    # The file is for our platform, continuing
                     file_url = (link_data['url'])
                     file_path_declar = (link_data['path'])
                     file_name = (link_data['name'])
@@ -50,9 +43,6 @@ class LinkParser():
                     file_archive = (link_data['archive'])
                     file_hash =  (link_data['hash'])
 
-                    # file_path_declar is a special case, the file only provides a flag to what the directory should be, not an actual path
-                    # we will convert the flag into a directory string, and then combine the directroy and file_name
-                    # We will also define just the path to the file
                     if file_path_declar == BASE_FILEPATH_D:
                         file_path = BASE_FILEPATH_S + file_name
                         file_dir = BASE_FILEPATH_S
@@ -60,15 +50,12 @@ class LinkParser():
                         file_path = RESOURCE_FILEPATH_S + file_name
                         file_dir = RESOURCE_FILEPATH_D
                     else:
-                        # We don't know what happend?! set it to base directory!!!
                         file_path = BASE_FILEPATH_S + file_name
                         file_dir = BASE_FILEPATH_S
 
-                    # We need to append all files to the download list
                     self.download_list.append(
                         [file_url, file_path, file_name, file_extension, file_archive, file_hash, file_dir])
 
-                    # The file is a special case so we will append to the unzip_list to deal with later
                     if file_archive == True:
                         self.unzip_list.append(
                             [file_url, file_path, file_name, file_extension, file_archive, file_hash, file_dir])
